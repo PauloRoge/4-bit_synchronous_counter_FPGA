9dcb2d497a48f8f7d4a76a90177e6a195d22d030
Paulo Rogério <paulocandido815@gmail.com>
2023-10-21 10:46:02 -0300

    teste

---
+++ contador_4_bits.vhd
@@ -0,0 +1,17 @@
+library library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity contador_4_bits is
+    port (
+        
+    );
+end entity contador_4_bits;
+
+architecture hardware of contador_4_bits is
+    
+begin
+    
+    
+    
+end architecture hardware;
